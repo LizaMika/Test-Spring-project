4b34ef8115b10bbc6ed74473704791531927724f
Лизок <liza.mikulenko@mail.ru>
2024-10-11 16:02:12 +0300

    add bd

--- src/main/java/com/example/demo/User.java
+++ src/main/java/com/example/demo/User.java
@@ -1,11 +1,22 @@
 package com.example.demo;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
     private String name;
+    @Column(name = "surname")
     private String surname;
 
     public User(String name, String surname) {
